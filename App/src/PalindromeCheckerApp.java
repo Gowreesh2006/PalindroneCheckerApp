2d7f16b280e46ec7c486097c9b08b70ab4f36a5b
GOWREESH <katagowreesh18@gmail.com>
2026-02-25 15:43:20 +0530

    GOWREESH Added UC7 Deque Based Palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -9,30 +9,27 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a word: ");
         String input = sc.nextLine();
 
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> deque = new LinkedList<>();
 
-        // Enqueue + Push characters
+        // Insert into deque
         for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            queue.add(ch);   // FIFO
-            stack.push(ch);  // LIFO
+            deque.addLast(input.charAt(i));
         }
 
         boolean isPalindrome = true;
 
-        // Compare dequeue vs pop
-        while (!queue.isEmpty()) {
-            if (queue.remove() != stack.pop()) {
+        // Compare front and rear
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
                 isPalindrome = false;
                 break;
             }
         }
 
         if (isPalindrome) {
-            System.out.println("Palindrome (Queue + Stack Method)");
+            System.out.println("Palindrome (Deque Method)");
         } else {
-            System.out.println("Not Palindrome (Queue + Stack Method)");
+            System.out.println("Not Palindrome (Deque Method)");
         }
 
         sc.close();
