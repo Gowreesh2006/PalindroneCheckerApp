b184b3fcb835da34a410f465d4cab29999e3f5c2
GOWREESH <katagowreesh18@gmail.com>
2026-02-25 15:50:48 +0530

    GOWREESH Added UC8 Linked List Palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -11,14 +11,20 @@ public class PalindromeCheckerApp {
 
         Deque<Character> deque = new LinkedList<>();
 
+<<<<<<< HEAD
         // Insert into deque
+=======
+>>>>>>> 3d63a90faa9fcb8b0374b84957df6cb8a70381aa
         for (int i = 0; i < input.length(); i++) {
             deque.addLast(input.charAt(i));
         }
 
         boolean isPalindrome = true;
 
+<<<<<<< HEAD
         // Compare front and rear
+=======
+>>>>>>> 3d63a90faa9fcb8b0374b84957df6cb8a70381aa
         while (deque.size() > 1) {
             if (deque.removeFirst() != deque.removeLast()) {
                 isPalindrome = false;
